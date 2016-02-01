1e6744c9d127fdef066d6b00804e5ab3fd1e5425
kasselTrankos <alvaro.touzon@gmail.com>
2016-02-01 23:13:23 +0100

    two test TDD

--- src/app/twitter/util/Pagination.js
+++ src/app/twitter/util/Pagination.js
@@ -1,11 +1,28 @@
-module.exports = function(tweets, tweetPerPage){
+module.exports = function(tweets, tweetPerPage, showPagesPagination){
   var Tweets = tweets || false;
-  var TweetPerPage = TweetPerPage || 10;
-
-  function GetPages() {
+  var TweetPerPage = tweetPerPage || 10;
+  var ShowPagesPagination = showPagesPagination || 10;
+  var currentPage = 1;
+  function getPages() {
     return Math.ceil(Tweets/TweetPerPage);
   }
+  function setActualPage(page){
+    currentPage = page;
+    return this;
+  }
+ function getFirstPage(){
+    return (lessThanMiddle()) ? currentPage : Math.ceil(currentPage-getMiddlePage());
+  }
+
+  function lessThanMiddle(){
+    return (currentPage<=getMiddlePage());
+  }
+  function getMiddlePage(){
+    return Math.ceil(showPagesPagination/2);
+  }
   return {
-    GetPages: GetPages
+    getPages: getPages,
+    setActualPage: setActualPage,
+    getFirstPage: getFirstPage
   }
 }
--- test/twitter.test.js
+++ test/twitter.test.js
@@ -1,11 +1,19 @@
 var expect    = require("chai").expect;
 var Pagination = require('./../src/app/twitter/util/Pagination');
+var Tweets, TweetsByPage, ShowPagesPagination;
+beforeEach(function() {
+  Tweets = 181;
+  TweetsByPage = 10;
+  ShowPagesPagination = 6;
+});
 describe('Pagination for Tweets', function() {
   describe('Tweets to pages', function(){
-    it("converts tweets to pages", function(){
-      var Tweets = 181;
-      var TweetsByPage = 10;
-      expect(Pagination(Tweets, TweetsByPage).GetPages()).to.equal(19);
+    it("Given 181 tweets expects 19 pages(max. 10 tweets by page)", function(){
+      expect(Pagination(Tweets, TweetsByPage).getPages()).to.equal(19);
+    });
+    it("Expect first page of pagination the 3, when page actual is 3 or 6", function(){
+      expect(Pagination(Tweets, TweetsByPage, ShowPagesPagination)
+      .setActualPage(6).getFirstPage()).to.equal(3);
     });
   });
 });
